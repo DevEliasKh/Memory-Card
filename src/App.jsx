27d07ed3c05203767e5e7f24cd034f5eca91cbc5
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2023-08-24 08:36:00 +0330

    finish the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 import StartGame from './component/startGame';
+import Footer from './component/footer';
 
 function App() {
 	return (
 		<div className="container">
 			<StartGame />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/component/footer.jsx
@@ -0,0 +1,12 @@
+import '../style/footer.css';
+
+function Footer() {
+	return (
+		<footer>
+			<div>Made by&nbsp;</div>
+			<a href="https://github.com/DevEliasKh"> DevEliasKh</a>
+		</footer>
+	);
+}
+
+export default Footer;
